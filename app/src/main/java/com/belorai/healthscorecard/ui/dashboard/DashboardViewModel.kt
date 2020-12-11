9c49a5f12df8e097c37f0e5cc000f8e4b8aec977
Steven McAlpine <stmcalpine@hotmail.com>
2020-12-11 14:18:41 +0300

    initial commit

---
+++ app/src/main/java/com/belorai/healthscorecard/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.belorai.healthscorecard.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
